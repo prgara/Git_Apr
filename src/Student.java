4d6b6d838191406fdd932d5a484e06eac82587a5
Lovepreet <pragra48@gmail.com>
2026-05-28 20:29:13 -0400

    added getter & setters

--- src/Student.java
+++ src/Student.java
@@ -4,4 +4,25 @@ public class Student {
     String name;
     int id;
 
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
